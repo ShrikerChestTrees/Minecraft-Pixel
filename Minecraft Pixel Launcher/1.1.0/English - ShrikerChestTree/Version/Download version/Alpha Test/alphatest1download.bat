3ea8fb588653d8bd1d96706f0821ce4e5d42ea90
ShrikerChestTree <163498687+ShrikerChestTrees@users.noreply.github.com>
2025-04-10 01:58:46 +0300

    Update alphatest1download.bat

--- Minecraft Pixel Launcher/1.1.0/English - ShrikerChestTree/Version/Download version/Alpha Test/alphatest1download.bat
+++ Minecraft Pixel Launcher/1.1.0/English - ShrikerChestTree/Version/Download version/Alpha Test/alphatest1download.bat
@@ -1,2 +1,2 @@
 $client = new-object System.Net.WebClient
-$client.DownloadFile("http://www.xyz.net/file.txt","C:\tmp\file.txt")
+$client.DownloadFile("https://github.com/ShrikerChestTrees/Minecraft-Pixel/archive/refs/heads/alphatest1eng.zip","C:\Users\%username%\Desktop\Minecraft Pixel Launcher\English - ShrikerChestTree\Version\alphatest1eng.zip")
